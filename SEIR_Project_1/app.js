1903d644251c2c17837094b8e7d4b2db67de83cd
Vikram Sharma <vikram.sharma1024@gmail.com>
2020-02-08 23:43:58 -0500

    initial commit

--- SEIR_Project_1/app.js
+++ SEIR_Project_1/app.js
@@ -1,3 +1,9 @@
 console.log($);
 console.log('have faith in me nigga');
 // TODO:
+/*
+1. create ajax call for each character
+2. append API data to character-container
+3. add the click to hide/show functionality
+4. add character image and format link
+*/
