8be0e4542d8feb5e41a400dd597b8c7b6a783cf7
Vikram Sharma <vikram.sharma1024@gmail.com>
2020-02-09 09:16:46 -0500

    ajax requests created, html boilerplate started

--- SEIR_Project_1/app.js
+++ SEIR_Project_1/app.js
@@ -1,9 +1,57 @@
-console.log($);
-console.log('have faith in me nigga');
+
 // TODO:
 /*
-1. create ajax call for each character
 2. append API data to character-container
 3. add the click to hide/show functionality
 4. add character image and format link
 */
+$.ajax({
+    url:'https://swapi.co/api/people/32'
+}).then(
+    (data) => {
+        console.log(data);
+    },
+    () => {
+        console.log('This isn\'t Tatooine!');
+    }
+)
+$.ajax({
+    url:'https://swapi.co/api/people/11'
+}).then(
+    (data) => {
+        console.log(data);
+    },
+    () => {
+        console.log('This isn\'t Tatooine!');
+    }
+)
+$.ajax({
+    url:'https://swapi.co/api/people/44'
+}).then(
+    (data) => {
+        console.log(data);
+    },
+    () => {
+        console.log('This isn\'t Tatooine!');
+    }
+)
+$.ajax({
+    url:'https://swapi.co/api/people/67'
+}).then(
+    (data) => {
+        console.log(data);
+    },
+    () => {
+        console.log('This isn\'t Tatooine!');
+    }
+)
+$.ajax({
+    url:'https://swapi.co/api/people/79'
+}).then(
+    (data) => {
+        console.log(data);
+    },
+    () => {
+        console.log('This isn\'t Tatooine!');
+    }
+)
