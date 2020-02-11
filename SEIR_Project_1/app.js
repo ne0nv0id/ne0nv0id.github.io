2e7bcd3f2049c871a9385e20bcaeef25b11ec2d5
Vikram Sharma <vikram.sharma1024@gmail.com>
2020-02-10 23:06:22 -0500

    appending ajax info to divs

--- SEIR_Project_1/app.js
+++ SEIR_Project_1/app.js
@@ -2,14 +2,34 @@
 // TODO:
 /*
 2. append API data to character-container
+    a. First, pull specific key/value pairs from API and log them
+    b. Append pairs to char info divs
+    c. Use template literals to append the pairs inside of sentences and organize the information
 3. add the click to hide/show functionality
-4. add character image and format link
+    a. consider using one function and passing the current and next div as parameters
+    b. display none to current div and display block to next div
+4. lol styling
 */
+const $body = $('body');
+const $container = $('<div>').attr('id', 'flex-container');
+$(() => {
+    const $charDiv = $('<div>').addClass('character-section').css('display', 'none');
+    $('body').append($charDiv);
+})
+
 $.ajax({
     url:'https://swapi.co/api/people/32'
 }).then(
     (data) => {
-        console.log(data);
+        // console.log(data);
+        console.log(data.name)
+        const $anakinInfo = $('<div>')
+            .addClass('character-info')
+            .attr('id', 'anakin-info')
+            .text(data.name + data.birth_year);
+        console.log($anakinInfo);
+        $('#anakin').append($anakinInfo);
+
     },
     () => {
         console.log('This isn\'t Tatooine!');
@@ -19,7 +39,7 @@ $.ajax({
     url:'https://swapi.co/api/people/11'
 }).then(
     (data) => {
-        console.log(data);
+        // console.log(data);
     },
     () => {
         console.log('This isn\'t Tatooine!');
@@ -29,7 +49,7 @@ $.ajax({
     url:'https://swapi.co/api/people/44'
 }).then(
     (data) => {
-        console.log(data);
+        // console.log(data);
     },
     () => {
         console.log('This isn\'t Tatooine!');
@@ -39,7 +59,7 @@ $.ajax({
     url:'https://swapi.co/api/people/67'
 }).then(
     (data) => {
-        console.log(data);
+        // console.log(data);
     },
     () => {
         console.log('This isn\'t Tatooine!');
@@ -49,7 +69,7 @@ $.ajax({
     url:'https://swapi.co/api/people/79'
 }).then(
     (data) => {
-        console.log(data);
+        // console.log(data);
     },
     () => {
         console.log('This isn\'t Tatooine!');
