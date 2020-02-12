49d8b2e92ad3cf96094a97af997c2380fb6386a8
Vikram Sharma <vikram.sharma1024@gmail.com>
2020-02-12 18:35:24 -0500

    final styling changes and error message commit

--- SEIR_Project_1/app.js
+++ SEIR_Project_1/app.js
@@ -15,6 +15,7 @@ const $container = $('#flex-container');
 $(() => {
     $flexContainer = $('#flex-container');
     $('.character-section').css('display', 'none');
+    $('#about-info').css('background-color', 'black');
     $('#about').css('display', 'block');
     //about button on click function
     $('#aboutBtn').on('click', (event) => {
@@ -27,20 +28,25 @@ $(() => {
     $('#anakinBtn').on('click', (event) => {
         event.preventDefault();
         $('.character-section').css('display', 'none');
+        console.log('anakin click');
         $.ajax({
             url:'https://swapi.co/api/people/11'
         }).then(
             (data) => {
                 console.log(data.name);
                 $anakinInfo = $('#anakin-info');
+                $anakinInfo.css('background-color', 'black');
                 $anakinInfo.text(
                     `${data.name}, born in ${data.birth_year}, played many roles in the Star Wars universe. From starting a protege with budding potential to ending as the chosen one to bring down the Empire, he remains my favorite charcter to this day. He was and still is regarded as the strongest force user to live aside from Yoda himself.`
                 );
                 $anakin.css('display', 'block');
-                // $flexContainer.append($anakin);
             },
             () => {
                 console.log('This isn\'t Tatooine!');
+                $anakinInfo = $('#anakin-info');
+                $anakinInfo.css('background-color', 'black');
+                $anakinInfo.text(`API unable to load, data was not able to be pulled`)
+                $anakin.css('display', 'block')
             }
         )
 
@@ -55,14 +61,18 @@ $(() => {
             (data) => {
                 console.log(data.name);
                 $quiGonInfo = $('#quigon-info');
+                $quiGonInfo.css('background-color', 'black');
                 $quiGonInfo.text(
                     `${data.name}, born in ${data.birth_year}, was a Jedi master seen in Star Wars Episode I. His apprentice was the legendary Obi Wan Kenobi, and he was very learned with the Force and the Jedi way. Up until his end fighing Darth Maul, he carried his calm and cool demeanor.`
                 );
                 $quiGon.css('display', 'block');
-                // $flexContainer.append($anakin);
             },
             () => {
                 console.log('This isn\'t Tatooine!');
+                $quiGonInfo = $('#quigon-info');
+                $quiGonInfo.css('background-color', 'black');
+                $quiGonInfo.text(`API unable to load, data was not able to be pulled`)
+                $quiGon.css('display', 'block')
             }
         )
 
@@ -77,6 +87,7 @@ $(() => {
             (data) => {
                 console.log(data.name);
                 $darthMaulInfo = $('#darthmaul-info');
+                $darthMaulInfo.css('background-color', 'black');
                 $darthMaulInfo.text(
                     `${data.name}, born in ${data.birth_year}, was the ominous agent of the Sith during Episode I. From the planet of Dathomir, much of Darth Maul's origin is explained in the extended literature including his brutally aggressive combat style. Brutal enough to bring down Qui-Gon Jinn.`
                 );
@@ -85,6 +96,10 @@ $(() => {
             },
             () => {
                 console.log('This isn\'t Tatooine!');
+                $darthMaulInfo = $('#darthmaul-info');
+                $darthMaulInfo.text(`API unable to load, data was not able to be pulled`)
+                $darthMaulInfo.css('background-color', 'black');
+                $darthMaul.css('display', 'block')
             }
         )
     })
@@ -98,6 +113,7 @@ $(() => {
             (data) => {
                 console.log(data.name);
                 $grievousInfo = $('#grievous-info');
+                $grievousInfo.css('background-color', 'black');
                 $grievousInfo.text(
                     `General ${data.name}, birth year ${data.birth_year}, is the only one in this list that had no capabilities with the force. Not much is known about him, but Grievous belonged to a warrior tribe of hominid aliens, and was equipped with his cybernetic parts by the Empire - sent after Republic/Jedi targets by Dooku himself.`
                 );
@@ -106,6 +122,10 @@ $(() => {
             },
             () => {
                 console.log('This isn\'t Tatooine!');
+                $grievousInfonInfo = $('#grievous-info');
+                $grievousInfo.css('background-color', 'black');
+                $grievousInfo.text(`API unable to load, data was not able to be pulled`)
+                $grievous.css('display', 'block')
             }
         )
     })
@@ -119,6 +139,7 @@ $(() => {
             (data) => {
                 console.log(data.name);
                 $dookuInfo = $('#dooku-info');
+                $dookuInfo.css('background-color', 'black');
                 $dookuInfo.text(
                     `${data.name} Tyrannus, born in ${data.birth_year}, was Palpatine's direct disciple for Episodes I-III. Dooku, while powerful, continued to disobey the Sith's Rule of Two which led Palpatine to believe Dooku would attempt a coup. Therefore, Palpatine orchestrated Dooku's assassination in Episode 3.`
                 );
@@ -127,6 +148,10 @@ $(() => {
             },
             () => {
                 console.log('This isn\'t Tatooine!');
+                $dookuInfo = $('#dooku-info');
+                $dookuInfo.css('background-color', 'black');
+                $dookuInfo.text(`API unable to load, data was not able to be pulled`)
+                $dooku.css('display', 'block')
             }
         )
 
